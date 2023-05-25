1eba97060c70e2e96c6abbd517aae90e465f8853
Nishan <109782567+nishan979@users.noreply.github.com>
2023-05-25 23:25:45 +0600

    Update ComCheckAlphabet.java

---
+++ Check Alphabet/com/check/alphabet/ComCheckAlphabet.java
@@ -0,0 +1,31 @@
+//package com.check.alphabet;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nishan
+ */
+public class ComCheckAlphabet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char ch;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any character: ");
+        ch = scan.next().charAt(0);
+//        System.out.println(ch);
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            System.out.println("This is a character.");
+        } 
+        else if (ch >= '0' && ch <= '9');
+        System.out.println("This is a number.");
+        else {
+                 System.out.println("This a special character.");
+             }
+    }
+
+}
