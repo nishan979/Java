af62a562657ddc85060880c98efe111ed163f947
Nishan <109782567+nishan979@users.noreply.github.com>
2023-05-26 22:53:52 +0600

    Update PrimeNumber.java

--- Prime Number/PrimeNumber/PrimeNumber.java
+++ Prime Number/PrimeNumber/PrimeNumber.java
@@ -1,24 +1,26 @@
 package PrimeNumber;
 
-import java.util.Scanner;
-
 public class PrimeNumber {
-    
+
     int i, j, count;
 
-    System.out.println("Enter upto which no. u want: ");
-    Scanner scan = new Scanner(System.in);
-    int range = scan.nextInt();
-    System.out.println("Primen numbers are: ");
-    for (i = 2; i <= range; i++) {
-        count = 0;
-        for (j = 2; j <= i; j++) {
-            if (i % j == 0) {
-                count++;
+    void show(int x) {
+        System.out.println("Prime numbers are: ");
+        for (i = 2; i <= x; i++) {
+            count = 0;
+            for (j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.print(i + " ");
             }
         }
-        if (count == 1) {
-            System.out.print(i + " ");
-        }
     }
+
 }
+
+public static void main(String[] args) {
+    PrimeNumber P = new PrimeNumber();
+    P.show(100);}
