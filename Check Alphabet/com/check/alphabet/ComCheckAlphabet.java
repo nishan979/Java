9331caf7d037664d093c9298c384d23c429b030b
Nishan <109782567+nishan979@users.noreply.github.com>
2023-05-26 22:53:39 +0600

    Update ComCheckAlphabet.java

--- Check Alphabet/com/check/alphabet/ComCheckAlphabet.java
+++ Check Alphabet/com/check/alphabet/ComCheckAlphabet.java
@@ -17,15 +17,17 @@ public class ComCheckAlphabet {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter any character: ");
         ch = scan.next().charAt(0);
-//        System.out.println(ch);
+        
         if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
             System.out.println("This is a character.");
         } 
-        else if (ch >= '0' && ch <= '9');
-        System.out.println("This is a number.");
-        else {
-                 System.out.println("This a special character.");
-             }
+        else if (ch >= '0' && ch <= '9');{
+             System.out.println("This is a number.");
+        }
+       else{
+        System.out.println("This a special character.");
+       }
+        
     }
 
 }
