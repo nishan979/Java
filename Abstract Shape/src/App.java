feaab946b75fe751ebc13d8c3394ed63e9c515cb
Nishan <109782567+nishan979@users.noreply.github.com>
2023-06-03 02:30:38 +0600

    Implement the right code

--- Abstract Shape/src/App.java
+++ Abstract Shape/src/App.java
@@ -3,62 +3,67 @@ abstract class Shape {
 
     abstract void draw();
 
-   // abstract void area(int length, int width);
-   // abstract void area(int rad);
     abstract void area();
 
-    
-
-   // abstract void perimeter(int length, int width);
-    //abstract void perimeter(int rad);
     abstract void perimeter();
 }
 
 class Rectangle extends Shape {
-    
-    public void draw() {
+    int length, width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    void draw() {
         System.out.println("This is Rectangle.");
     }
 
-    public void area(int length, int width) {
+    void area() {
         int recArea = length * width;
         System.out.println("Area of this rectangle: " + recArea);
     }
 
-    public void perimeter(int length, int width) {
+    public void perimeter() {
         int perArea = 2 * (length + width);
         System.out.println("Perimeter of this rectangle: " + perArea);
     }
 
 }
 
-// class Circle extends Shape {
-//     // int length, width;
-//     void draw() {
-//         System.out.println("This is Circle.");
-//     }
+class Circle extends Shape {
+    int rad;
 
-//     void area(int rad) {
-//         double cirArea = 3.1416 * rad * rad;
-//         System.out.println("Area of this Circle: " + cirArea);
-//     }
+    public Circle(int rad) {
+        this.rad = rad;
+    }
 
-//     void perimeter(int rad) {
-//         double cirArea = 2 * 3.1416 * rad;
-//         System.out.println("Perimeter of this Circle: " + cirArea);
-//     }
+    void draw() {
+        System.out.println("This is Circle.");
+    }
 
-// }
+    void area() {
+        double cirArea = 3.1416 * rad * rad;
+        System.out.println("Area of this Circle: " + cirArea);
+    }
+
+    void perimeter() {
+        double cirArea = 2 * 3.1416 * rad;
+        System.out.println("Perimeter of this Circle: " + cirArea);
+    }
+
+}
 
 public class App {
     public static void main(String[] args) {
-        Rectangle ob1 = new Rectangle();
+        Rectangle ob1 = new Rectangle(2, 4);
         ob1.draw();
-        ob1.area(2, 4);
-        ob1.perimeter(2, 4);
-        Circle cir1 = new Circle();
+        ob1.area();
+        ob1.perimeter();
+        Circle cir1 = new Circle(3);
         cir1.draw();
-        cir1.area(3);
-        cir1.perimeter(3);
+        cir1.area();
+        cir1.perimeter();
     }
 }
