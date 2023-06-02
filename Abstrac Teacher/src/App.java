e4bf47da17edf266518325570bf31d73ef84d988
Nishan <109782567+nishan979@users.noreply.github.com>
2023-06-02 13:09:08 +0600

    Write the code

--- Abstrac Teacher/src/App.java
+++ Abstrac Teacher/src/App.java
@@ -1,5 +1,18 @@
+
+abstract class Teacher{
+    abstract void show();
+}
+
+class CSETeacher extends Teacher{
+    void show(){
+        System.out.println("I am in CSETeacher.");
+    }
+}
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CSETeacher ob = new CSETeacher();
+        ob.show();
     }
 }
