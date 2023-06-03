d25ee3d60f5ef83436a42417dab51d308a24c769
Nishan <109782567+nishan979@users.noreply.github.com>
2023-06-04 03:08:30 +0600

    Interface Problem 2

---
+++ Interface M/src/App.java
@@ -0,0 +1,41 @@
+interface myinterface {
+    abstract void m1();
+
+    abstract void m2();
+
+    abstract void m3();
+
+    abstract void m4();
+
+}
+
+class Myclass1 implements myinterface {
+    public void m1() {
+        System.out.println("I am in M1");
+    }
+
+    public void m2() {
+        System.out.println("I am in M2");
+    }
+
+    public void m3() {
+        System.out.println("I am in M3");
+    }
+
+    public void m4() {
+        System.out.println("I am in M4");
+    }
+
+}
+
+class Myclass2 implements myinterface{
+    public void m2(){
+        System.out.println("I am in M2 of Myclass2");
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
