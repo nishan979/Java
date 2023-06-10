55cb26ba276f299681715bca7164e62ce58d5799
Nishan <109782567+nishan979@users.noreply.github.com>
2023-06-10 13:27:25 +0600

    Wrote code

--- Interface Problem 2/src/App.java
+++ Interface Problem 2/src/App.java
@@ -1,5 +1,18 @@
+import java.net.Socket;
+
+class Teacher{
+     public void show(){
+        System.out.println("I am in Teacher.");
+     }
+}
+
+class CSETeacher extends Teacher{
+
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CSETeacher ob1 = new CSETeacher();
+        ob1.show();
     }
 }
